db1f189500ac2d10cf3c035c48f59640b43400ad
Adrian <adicirstei@gmail.com>
2015-04-16 09:35:36 +0300

    added ps profile file

---
+++ ps/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,7 @@
+Import-Module PsGet
+Import-Module D:\soft\PSModules\nvm\nvm.psm1
+# Load posh-git example profile
+. 'D:\soft\PSModules\posh-git\profile.example.ps1'
+
+. "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /x86
+. "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
