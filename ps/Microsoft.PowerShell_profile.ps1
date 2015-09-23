09ef87b0d2c7a93d4a57f1074a423c19950a0be0
Adrian <adicirstei@gmail.com>
2015-09-23 11:56:42 +0300

    added which function

--- ps/Microsoft.PowerShell_profile.ps1
+++ ps/Microsoft.PowerShell_profile.ps1
@@ -4,6 +4,11 @@ Import-Module D:\soft\PSModules\nvm
 . "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /x86
 . "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
 
+function which {
+  Param ([String]$Name)
+  (Get-Command $Name).Definition
+}
+
 function lastnode {
     set-nodeversion $(@(get-nodeversions) | Sort-object | select-object -last 1)
     "Node version $(node -v)"
