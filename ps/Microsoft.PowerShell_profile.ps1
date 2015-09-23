666d2956fd8664da69960e862293dd0466568f6c
Adrian <adicirstei@gmail.com>
2015-09-23 10:05:18 +0300

    merged profile script

--- ps/Microsoft.PowerShell_profile.ps1
+++ ps/Microsoft.PowerShell_profile.ps1
@@ -1,9 +1,6 @@
 Import-Module PsGet
 Import-Module D:\soft\PSModules\nvm
 
-# Load posh-git example profile
-. 'D:\soft\PSModules\posh-git\profile.example.ps1'
-
 . "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /x86
 . "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
 
@@ -17,4 +14,3 @@ function lastio {
     "Node version $(node -v)"
 }
 lastnode
-lastio
