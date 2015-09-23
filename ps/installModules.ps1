fe7c87e0ef628f89438c762203a242bebf20cc81
unknown <adrian_cirstei@ROBUCACIRSTL10.cotyww.com>
2015-09-23 12:02:25 +0300

    added instalation steps for PsGet and posh-git modules@

--- ps/installModules.ps1
+++ ps/installModules.ps1
@@ -1,2 +1,9 @@
 New-Item $(Join-Path $script:installDir PSModules) -type directory -ea SilentlyContinue
+
+# Install PsGet
+(new-object Net.WebClient).DownloadString("http://psget.net/GetPsGet.ps1") | iex
+
+install-module nvm
+install-module posh-git
+
 write-host "Installed PowerShell Modules"
