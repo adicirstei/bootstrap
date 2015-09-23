2fb4f27d6474d42f26a8e5a25c7dcf907368cd24
Adrian <adicirstei@gmail.com>
2015-09-23 10:01:36 +0300

    support for consolez

--- ps/installConsole2.ps1
+++ ps/installConsole2.ps1
@@ -1,6 +1,6 @@
 $script:consoleDir = Join-Path $script:installDir Console2
 
 New-Item $script:consoleDir -type directory -ea SilentlyContinue
-copy-item .\bin\console.*, .\bin\FreeImage*.*, *.ico -destination $script:consoleDir
+copy-item .\bin\console*.*, .\bin\FreeImage*.*, *.ico -destination $script:consoleDir
 
 write-host "Installed Console2"
